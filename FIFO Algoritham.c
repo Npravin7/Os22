6892f312fbf8cfe0ec1b194baab7d76b52a8c8c2
Npravin7 <110155073+Npravin7@users.noreply.github.com>
2022-07-28 12:14:05 +0530

    Create FIFO Algoritham.c

---
+++ FIFO Algoritham.c
@@ -0,0 +1,59 @@
+# Os22
+FiFO Algorthim   using page replacement
+DESCRIPTIOPN: FIFO is the simplest among all algorithms which are responsible for maintaining all the pages in a queue for an operating system and also keeping track of all the pages in a queue.
+The older pages are kept in the front and the newer ones are kept at the end of the queue. Pages that are in the front are removed first and the pages which are demanded are added.
+
+#include <stdio.h>
+int main()
+{
+    int incomingStream[] = {4, 1, 2, 4, 5};
+    int pageFaults = 0;
+    int frames = 3;
+    int m, n, s, pages;
+
+    pages = sizeof(incomingStream)/sizeof(incomingStream[0]);
+
+    printf("Incoming \t Frame 1 \t Frame 2 \t Frame 3");
+    int temp[frames];
+    for(m = 0; m < frames; m++)
+    {
+        temp[m] = -1;
+    }
+
+    for(m = 0; m < pages; m++)
+    {
+        s = 0;
+
+        for(n = 0; n < frames; n++)
+        {
+            if(incomingStream[m] == temp[n])
+            {
+                s++;
+                pageFaults--;
+            }
+        }
+        pageFaults++;
+        
+        if((pageFaults <= frames) && (s == 0))
+        {
+            temp[m] = incomingStream[m];
+        }
+        else if(s == 0)
+        {
+            temp[(pageFaults - 1) % frames] = incomingStream[m];
+        }
+      
+        printf("\n");
+        printf("%d\t\t\t",incomingStream[m]);
+        for(n = 0; n < frames; n++)
+        {
+            if(temp[n] != -1)
+                printf(" %d\t\t\t", temp[n]);
+            else
+                printf(" - \t\t\t");
+        }
+    }
+
+    printf("\nTotal Page Faults:\t%d\n", pageFaults);
+    return 0;
+}
